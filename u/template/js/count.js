c09dc523dc99b697bcb8f5ed555f5584623dd335
h <h@huzoi.me>
2025-11-26 16:52:48 +0100

    Delete u/template/js/count.js

--- u/template/js/count.js
+++
@@ -1,9 +0,0 @@
-// counter.js
-(() => {
-  fetch(`https://hoodrich-counter.tear-7b0.workers.dev/hit?path=${encodeURIComponent(location.pathname)}`)
-    .then(r => r.json())
-    .then(j => {
-      const el = document.getElementById('vcn');
-      if (el) el.textContent = j.views;
-    });
-})();
