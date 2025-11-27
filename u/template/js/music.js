ce0c81a58627c0c80c69a9ebb3fadce5435e8a75
h <h@huzoi.me>
2025-11-27 04:45:33 +0100

    Update music.js

--- u/template/js/music.js
+++ u/template/js/music.js
@@ -3,17 +3,17 @@ const audio = document.getElementById('audioatp');
 
 if (button && audio) {
   let isPlaying = false;
-
+  button.innerHTML = '<i class="fas fa-volume-mute"></i>';
   button.addEventListener('click', (e) => {
     e.stopPropagation();
     
     if (isPlaying) {
       audio.pause();
-      button.innerHTML = '<i class="fas fa-volume-up"></i>';
+      button.innerHTML = '<i class="fas fa-volume-mute"></i>';
       isPlaying = false;
     } else {
       audio.play();
-      button.innerHTML = '<i class="fas fa-volume-mute"></i>';
+      button.innerHTML = '<i class="fas fa-volume-up"></i>';
       isPlaying = true;
     }
   });
