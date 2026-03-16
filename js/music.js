6bd2b29a1a6bcd4eab14304f6fe15c70fa668239
huzoi <huzoi@users.noreply.github.com>
2026-03-16 01:47:44 +0000

    deploy: 0b0513128144d90c5117d67b22b518a50b3074ef

--- js/music.js
+++ js/music.js
@@ -2,19 +2,16 @@ const button = document.getElementById('soundToggle');
 const audio = document.getElementById('audioatp');
 
 if (button && audio) {
-  let isPlaying = false;
   button.innerHTML = '<i class="fas fa-volume-mute"></i>';
   button.addEventListener('click', (e) => {
     e.stopPropagation();
     
-    if (isPlaying) {
+    if (!audio.paused) {
       audio.pause();
       button.innerHTML = '<i class="fas fa-volume-mute"></i>';
-      isPlaying = false;
     } else {
       audio.play();
       button.innerHTML = '<i class="fas fa-volume-up"></i>';
-      isPlaying = true;
     }
   });
 }
