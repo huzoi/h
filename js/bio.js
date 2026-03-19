40f72874bcea3cbf60f86fb33fcef0b8e256410d
h <github@huzoi.me>
2026-03-19 02:51:55 +0100

    Fix bioArray initialization by removing extra string
    Removed extra string from bioArray initialization.

--- js/bio.js
+++ js/bio.js
@@ -1,5 +1,5 @@
 const bioText = document.getElementById('bioText');
-const bioArray = window.BIO_ARRAY || ['Backend', 'Spain | 19', '@archlinux | GitLab Contributor', '@katanalinux [Kodachi Fork] (WIP / ETA 2028) | "Torrified OS, for everyone"'];
+const bioArray = window.BIO_ARRAY || ['Backend', 'Spain | 19', '@archlinux | GitLab Contributor', '@katanalinux [Kodachi Fork] (WIP / ETA 2028)'];
 
 let charIndex = 0;
 let bioIndex = 0;
